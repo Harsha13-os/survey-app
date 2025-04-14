7c0306b842d75142f691113a47852e6b1a62a818
HARSHAVARDHAN REDDY KOMMAREDDY <harshavardhankomma@gmail.com>
2025-04-14 17:56:14 -0400

    removed unwanted files

--- SWE642-HW3/backend/src/main/java/com/example/backend/StudentSurveyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend;
-
-import com.example.backend.StudentSurvey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentSurveyRepository extends JpaRepository<StudentSurvey, Long> {
-}
